7dba8e457227e2637c752e27038c3c3096e1cea3
bomaye01 <linus@kuehnle-ac.de>
2024-09-10 14:34:51 +0200

    link to normal task

--- load_task_group.go
+++ load_task_group.go
@@ -1,25 +1,64 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 )
 
+type KwdQuery struct {
+	rawQueryStr       string
+	inclusiveKeywords []string
+	exclusiveKeywords []string
+	orKeywordsGroups  [][]string
+}
+type SkuQuery string
+
+type QueryType int
+
+const (
+	SkuQueryType QueryType = iota
+	KwdQueryType QueryType = iota
+)
+
 type LoadTaskGroup struct {
 	*BaseTaskGroup
+	normalTaskGroup *NormalTaskGroup
 	lastKnownPid    string
 	skuQueryStrings []string
 	kwdQueryStrings []string
+	skuQueries      []SkuQuery
+	kwdQueries      []KwdQuery
 }
 
 func NewLoadTaskGroup(proxyHandler *ProxyHandler, webhookHandler *WebhookHandler, lastKnownPid string, skuQueryStrings []string, kwdQueryStrings []string) (*LoadTaskGroup, error) {
+	skuQueries := []SkuQuery{}
+	for _, queryStr := range skuQueryStrings {
+		queryStr = strings.ToUpper(queryStr)
+		queryStr = strings.TrimSpace(queryStr)
+
+		skuQueries = append(skuQueries, SkuQuery(queryStr))
+	}
+
+	kwdQueries := []KwdQuery{}
+	for _, queryStr := range kwdQueryStrings {
+		queryStr = strings.ToLower(queryStr)
+		queryStr = strings.TrimSpace(queryStr)
+
+		q := createKeywordQuery(queryStr)
+
+		kwdQueries = append(kwdQueries, q)
+	}
+
 	loadTaskGroup := &LoadTaskGroup{
 		lastKnownPid:    lastKnownPid,
 		skuQueryStrings: skuQueryStrings,
 		kwdQueryStrings: kwdQueryStrings,
+		skuQueries:      skuQueries,
+		kwdQueries:      kwdQueries,
 	}
 
-	baseTaskGroup, err := NewBaseTaskGroup(proxyHandler, webhookHandler)
+	baseTaskGroup, err := NewBaseTaskGroup(" LOAD ", proxyHandler, webhookHandler)
 	if err != nil {
 		return nil, fmt.Errorf("error creating base task group: %v", err)
 	}
@@ -27,6 +66,19 @@ func NewLoadTaskGroup(proxyHandler *ProxyHandler, webhookHandler *WebhookHandler
 	loadTaskGroup.BaseTaskGroup = baseTaskGroup
 
 	return loadTaskGroup, nil
+}
+
+func (g *LoadTaskGroup) LinkToNormalTaskGroup(normalTaskGroup *NormalTaskGroup) error {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	if normalTaskGroup == nil {
+		return errors.New("error linking normal task group to load task group: normal task group nil")
+	}
+
+	g.normalTaskGroup = normalTaskGroup
+
+	return nil
 }
 
 func (g *LoadTaskGroup) AddSkuQuery(skuStr string) {
@@ -84,6 +136,35 @@ func (g *LoadTaskGroup) RemoveKwdQuery(kwdStr string) {
 		g.kwdQueryStrings = append(g.kwdQueryStrings[:removeIndex], g.kwdQueryStrings[removeIndex+1:]...)
 	}
 }
+
+func (g *LoadTaskGroup) handleNewArrivalsResponse(res *NewArrivalsResponse) {
+	if res == nil || len(res.Response.ProductNodes) == 0 || g.normalTaskGroup == nil {
+		return
+	}
+
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	newSKUs := []SkuQuery{}
+
+	for _, productNode := range res.Response.ProductNodes {
+		if productNode.Pid == g.lastKnownPid {
+			break
+		}
+
+		newSKUs = append(newSKUs, SkuQuery(productNode.Sku))
+	}
+
+	g.lastKnownPid = res.Response.ProductNodes[0].Pid
+
+	g.normalTaskGroup.AddLoadSkuQueries(newSKUs)
+}
+
+func (g *LoadTaskGroup) handleSkuCheckResponse(productNodes []ProductData) {
+
+}
+
+func (t *LoadTaskGroup) notifyLoad(productData *ProductData) {}
 
 func createKeywordQuery(kwdSearchQuery string) KwdQuery {
 	kwdGroup := KwdQuery{
