034df3dcb9b57e16de42c72f63d4eea3260df584
bomaye01 <linus@kuehnle-ac.de>
2024-09-10 15:55:17 +0200

    changes

--- net_structs.go
+++ net_structs.go
@@ -16,96 +16,189 @@ type ProductNodeReference struct {
 }
 
 // req: products by skus
+type productsBySkuBody struct {
+	Query     string    `json:"query"`
+	Variables Variables `json:"variables"`
+}
+
+type Variables struct {
+	CurrencyCode string   `json:"currencyCode"`
+	IncludeTax   bool     `json:"includeTax"`
+	Skus         []string `json:"skus"`
+}
+
 type ProductsBySkusResponse struct {
-	Data Data `json:"data"`
-}
-
-type Data struct {
-	Site Site `json:"site"`
-}
-
-type Site struct {
-	Search Search `json:"search"`
-}
-
-type Search struct {
-	SearchProducts SearchProducts `json:"searchProducts"`
-}
-
-type SearchProducts struct {
-	Products ProductReference `json:"products"`
-}
-
-type ProductReference struct {
-	Edges []ProductEdge `json:"edges"`
-}
-
-type ProductEdge struct {
-	Node ProductNode `json:"node"`
+	Data struct {
+		Site struct {
+			Search struct {
+				SearchProducts struct {
+					Products struct {
+						Edges []struct {
+							Node     ProductNode `json:"node"`
+							Typename string      `json:"__typename"`
+						} `json:"edges"`
+						Typename string `json:"__typename"`
+					} `json:"products"`
+					Typename string `json:"__typename"`
+				} `json:"searchProducts"`
+				Typename string `json:"__typename"`
+			} `json:"search"`
+			Typename string `json:"__typename"`
+		} `json:"site"`
+	} `json:"data"`
 }
 
 type ProductNode struct {
-	Sku          string        `json:"sku"`
-	Name         string        `json:"name"`
-	Prices       Prices        `json:"prices"`
-	DefaultImage DefaultImage  `json:"defaultImage"`
-	CustomFields []CustomField `json:"customFields"`
-	MetaFields   []MetaField   `json:"metafields"`
-	Variants     []Variant     `json:"variants"`
-}
-
-type Prices struct {
-	Price Price `json:"price"`
-}
-
-type Price struct {
-	CurrencyCode string `json:"currencyCode"`
-	Value        int    `json:"value"`
-}
-
-type DefaultImage struct {
-	Url string `json:"url"`
-}
-
-type CustomField struct {
-	Edges []CustomFieldEdge `json:"edges"`
-}
-
-type CustomFieldEdge struct {
-	Node CustomFieldNode `json:"node"`
-}
-
-type CustomFieldNode struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
-}
-
-type MetaField struct {
-	Edges []MetaFieldEdge `json:"edges"`
-}
-
-type MetaFieldEdge struct {
-	Node MetaFieldNode `json:"node"`
-}
-
-type MetaFieldNode struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
-
-type Variant struct {
-	Edges []VariantEdge `json:"edges"`
-}
-
-type VariantEdge struct {
-	Node VariantNode `json:"node"`
-}
-
-type VariantNode struct {
-	MetaFields []MetaField `json:"metafields"`
-	Inventory  Inventory   `json:"inventory"`
-}
-
-type Inventory struct {
-	IsInStock bool `json:"isInStock"`
+	Name         string `json:"name"`
+	Sku          string `json:"sku"`
+	DefaultImage struct {
+		URL string `json:"url"`
+	} `json:"defaultImage"`
+	CustomFields struct {
+		Edges []struct {
+			Node struct {
+				Name  string `json:"name"`
+				Value string `json:"value"`
+			} `json:"node"`
+		} `json:"edges"`
+	} `json:"customFields"`
+	Metafields struct {
+		Edges []struct {
+			Node struct {
+				ID    string `json:"id"`
+				Key   string `json:"key"`
+				Value string `json:"value"`
+			} `json:"node"`
+		} `json:"edges"`
+	} `json:"metafields"`
+	AvailabilityV2 struct {
+		Status      string `json:"status"`
+		Description string `json:"description"`
+		Typename    string `json:"__typename"`
+	} `json:"availabilityV2"`
+	Categories struct {
+		Edges []struct {
+			Node struct {
+				Metafields struct {
+					Edges []struct {
+						Node struct {
+							EntityID int    `json:"entityId"`
+							Key      string `json:"key"`
+							Value    string `json:"value"`
+							Typename string `json:"__typename"`
+						} `json:"node"`
+						Typename string `json:"__typename"`
+					} `json:"edges"`
+					Typename string `json:"__typename"`
+				} `json:"metafields"`
+				ID       string `json:"id"`
+				EntityID int    `json:"entityId"`
+				Name     string `json:"name"`
+				Typename string `json:"__typename"`
+			} `json:"node"`
+			Typename string `json:"__typename"`
+		} `json:"edges"`
+		Typename string `json:"__typename"`
+	} `json:"categories"`
+	Prices struct {
+		Price struct {
+			CurrencyCode string `json:"currencyCode"`
+			Value        int    `json:"value"`
+			Typename     string `json:"__typename"`
+		} `json:"price"`
+		BasePrice struct {
+			CurrencyCode string `json:"currencyCode"`
+			Value        int    `json:"value"`
+			Typename     string `json:"__typename"`
+		} `json:"basePrice"`
+		SalePrice  interface{} `json:"salePrice"`
+		PriceRange struct {
+			Min struct {
+				CurrencyCode string `json:"currencyCode"`
+				Value        int    `json:"value"`
+				Typename     string `json:"__typename"`
+			} `json:"min"`
+			Max struct {
+				CurrencyCode string `json:"currencyCode"`
+				Value        int    `json:"value"`
+				Typename     string `json:"__typename"`
+			} `json:"max"`
+			Typename string `json:"__typename"`
+		} `json:"priceRange"`
+		Typename string `json:"__typename"`
+	} `json:"prices"`
+	Inventory struct {
+		IsInStock bool   `json:"isInStock"`
+		Typename  string `json:"__typename"`
+	} `json:"inventory"`
+	Description string `json:"description"`
+	Variants    struct {
+		Edges []struct {
+			Node struct {
+				EntityID int    `json:"entityId"`
+				ID       string `json:"id"`
+				Sku      string `json:"sku"`
+				Prices   struct {
+					BasePrice struct {
+						CurrencyCode string `json:"currencyCode"`
+						Value        int    `json:"value"`
+						Typename     string `json:"__typename"`
+					} `json:"basePrice"`
+					Price struct {
+						CurrencyCode string `json:"currencyCode"`
+						Value        int    `json:"value"`
+						Typename     string `json:"__typename"`
+					} `json:"price"`
+					SalePrice interface{} `json:"salePrice"`
+					Typename  string      `json:"__typename"`
+				} `json:"prices"`
+				Inventory struct {
+					ByLocation struct {
+						Edges    []interface{} `json:"edges"`
+						Typename string        `json:"__typename"`
+					} `json:"byLocation"`
+					IsInStock bool   `json:"isInStock"`
+					Typename  string `json:"__typename"`
+				} `json:"inventory"`
+				ProductOptions struct {
+					Edges []struct {
+						Node struct {
+							EntityID    int    `json:"entityId"`
+							Typename    string `json:"__typename"`
+							DisplayName string `json:"displayName"`
+							Values      struct {
+								Edges []struct {
+									Node struct {
+										EntityID int    `json:"entityId"`
+										Label    string `json:"label"`
+										Typename string `json:"__typename"`
+									} `json:"node"`
+									Typename string `json:"__typename"`
+								} `json:"edges"`
+								Typename string `json:"__typename"`
+							} `json:"values"`
+						} `json:"node"`
+						Typename string `json:"__typename"`
+					} `json:"edges"`
+					Typename string `json:"__typename"`
+				} `json:"productOptions"`
+				Metafields struct {
+					Edges []struct {
+						Node struct {
+							ID       string `json:"id"`
+							Key      string `json:"key"`
+							Value    string `json:"value"`
+							Typename string `json:"__typename"`
+						} `json:"node"`
+						Typename string `json:"__typename"`
+					} `json:"edges"`
+					Typename string `json:"__typename"`
+				} `json:"metafields"`
+				Typename string `json:"__typename"`
+			} `json:"node"`
+			Typename string `json:"__typename"`
+		} `json:"edges"`
+		Typename string `json:"__typename"`
+	} `json:"variants"`
+	Typename string `json:"__typename"`
 }
